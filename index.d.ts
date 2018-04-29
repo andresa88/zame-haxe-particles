6bc9f7476b7924767a8ba9f150af3294ac3fd7cf
Andres Alva <andres.alva@driverdigital.com>
2018-04-28 17:47:44 -0700

    Added index.d.ts, index.esm.js containg es2015 exports, and modified package.json to add sideEffects field for webpack tree shaking purposes

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+
+export { default as ParticleSystem } from './lib/org/zamedev/particles/ParticleSystem';
+export { default as DefaultParticleRenderer } from './lib/org/zamedev/particles/renderers/DefaultParticleRenderer';
+export { default as SpritesParticleRenderer } from './lib/org/zamedev/particles/renderers/SpritesParticleRenderer';
+export { default as TilemapParticleRenderer } from './lib/org/zamedev/particles/renderers/TilemapParticleRenderer';
+export { default as ParticleLoader } from './lib/org/zamedev/particles/loaders/ParticleLoader';
---
+++ index.esm.js
@@ -0,0 +1,15 @@
+
+
+export { default as ParticleSystem } from './lib/_gen/org/zamedev/particles/ParticleSystem';
+export { default as DefaultParticleRenderer } from './lib/_gen/org/zamedev/particles/renderers/DefaultParticleRenderer';
+export { default as SpritesParticleRenderer } from './lib/_gen/org/zamedev/particles/renderers/SpritesParticleRenderer';
+export { default as TilemapParticleRenderer } from './lib/_gen/org/zamedev/particles/renderers/TilemapParticleRenderer';
+export { default as ParticleLoader } from './lib/_gen/org/zamedev/particles/loaders/ParticleLoader';
+
+/*
+export { default as ParticleSystem } from './lib/_ts/org/zamedev/particles/ParticleSystem';
+export { default as DefaultParticleRenderer } from './lib/_ts/org/zamedev/particles/renderers/DefaultParticleRenderer';
+export { default as SpritesParticleRenderer } from './lib/_ts/org/zamedev/particles/renderers/SpritesParticleRenderer';
+export { default as TilemapParticleRenderer } from './lib/_ts/org/zamedev/particles/renderers/TilemapParticleRenderer';
+export { default as ParticleLoader } from './lib/_ts/org/zamedev/particles/loaders/ParticleLoader';
+*/
