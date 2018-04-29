b1de124f856a7484c6f93fa859bded6600ef2802
Andres Alva <andres.alva@driverdigital.com>
2018-04-28 20:51:31 -0700

    Update

--- lib/org/zamedev/particles/renderers/SpritesParticleRenderer.d.ts
+++ lib/org/zamedev/particles/renderers/SpritesParticleRenderer.d.ts
@@ -5,6 +5,8 @@ import ParticleSystem from "../ParticleSystem";
 import Sprite from "../../../../openfl/display/Sprite";
 
 declare class SpritesParticleRenderer extends Sprite implements ParticleSystemRenderer {
+
+  constructor(manualUpdate?:boolean);
   addParticleSystem(ps: ParticleSystem): ParticleSystemRenderer;
   removeParticleSystem(ps: ParticleSystem): ParticleSystemRenderer;
   update(): void;
--- scripts/convert-to-es2015.js
+++ scripts/convert-to-es2015.js
@@ -13,6 +13,8 @@ with
 
 export default
 
+and remove the $global lines at the top of all the js files
+
 under lib/_gen/ to make all the modules es2015 modules as opposed to commonjs modules. This then 
 allows webpack to properly tree shake our code. Any unused modules can be excluded 
 from the webpack generated js bundle.
@@ -21,6 +23,7 @@ from the webpack generated js bundle.
 
 //console.log('Replacing exports.default = with export default...');
 
+// Search for all the js files under lib/_gen/
 globby(['../lib/_gen/**/*.js']).then((paths) => {
 
   let updated = 0;
@@ -46,10 +49,19 @@ function replace(filePath) {
     return;
   }
   
-
+  // By replacing exports.default with export default we are converting the module 
+  // to an es2015 module from commonjs
   var result = content.replace(/exports\.default =/g, 'export default');
 
+  // We must remove these lines at the top of the generated js files or else when the
+  // webpack generated bundle is run in the browser we will encounter an "exports is not defined" 
+  // error. The variable "exports" is only availabe in commonjs modules.
+  result = result.replace('var $global = typeof window != "undefined" ? window : typeof global != "undefined" ? global : typeof self != "undefined" ? self : this', '');
+  result = result.replace('$global.Object.defineProperty(exports, "__esModule", {value: true});', '');
+  result = result.replace('Object.defineProperty(exports, "__esModule", {value: true});', '');
+  
   if (result == content) {
+    // Nothing was changed in the file
     return false;
   }
 
