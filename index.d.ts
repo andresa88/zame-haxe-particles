70720fed925d88640c1a7064c29dfde842d0dfff
Andres Alva <andres.alva@driverdigital.com>
2018-04-28 20:51:03 -0700

    Added export to index files

--- index.d.ts
+++ index.d.ts
@@ -3,4 +3,5 @@ export { default as ParticleSystem } from './lib/org/zamedev/particles/ParticleS
 export { default as DefaultParticleRenderer } from './lib/org/zamedev/particles/renderers/DefaultParticleRenderer';
 export { default as SpritesParticleRenderer } from './lib/org/zamedev/particles/renderers/SpritesParticleRenderer';
 export { default as TilemapParticleRenderer } from './lib/org/zamedev/particles/renderers/TilemapParticleRenderer';
+export { default as ParticleSystemRenderer } from './lib/org/zamedev/particles/renderers/ParticleSystemRenderer';
 export { default as ParticleLoader } from './lib/org/zamedev/particles/loaders/ParticleLoader';
--- index.esm.js
+++ index.esm.js
@@ -4,12 +4,5 @@ export { default as ParticleSystem } from './lib/_gen/org/zamedev/particles/Part
 export { default as DefaultParticleRenderer } from './lib/_gen/org/zamedev/particles/renderers/DefaultParticleRenderer';
 export { default as SpritesParticleRenderer } from './lib/_gen/org/zamedev/particles/renderers/SpritesParticleRenderer';
 export { default as TilemapParticleRenderer } from './lib/_gen/org/zamedev/particles/renderers/TilemapParticleRenderer';
+export { default as ParticleSystemRender } from './lib/_gen/org/zamedev/particles/renderers/ParticleSystemRenderer';
 export { default as ParticleLoader } from './lib/_gen/org/zamedev/particles/loaders/ParticleLoader';
-
-/*
-export { default as ParticleSystem } from './lib/_ts/org/zamedev/particles/ParticleSystem';
-export { default as DefaultParticleRenderer } from './lib/_ts/org/zamedev/particles/renderers/DefaultParticleRenderer';
-export { default as SpritesParticleRenderer } from './lib/_ts/org/zamedev/particles/renderers/SpritesParticleRenderer';
-export { default as TilemapParticleRenderer } from './lib/_ts/org/zamedev/particles/renderers/TilemapParticleRenderer';
-export { default as ParticleLoader } from './lib/_ts/org/zamedev/particles/loaders/ParticleLoader';
-*/
